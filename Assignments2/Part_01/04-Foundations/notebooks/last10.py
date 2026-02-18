b14519999a70b6abb5a5e5a5c4e6a7b9a31ffdbe
nazmunmansur <mansurnazmun@gmail.com>
2026-02-18 16:22:44 +0000

    jupyter

---
+++ Assignments2/Part_01/04-Foundations/notebooks/last10.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+get_ipython().run_line_magic('history', '-n 1-5  # Show the first 5 commands in this session')
+# Save history to a file
+get_ipython().run_line_magic('save', 'my_session.py 1-5')
+get_ipython().run_line_magic('history', '-n 1-5  # Show the first 5 commands in this session')
+# Save history to a file
+get_ipython().run_line_magic('save', 'my_session.py 1-5')
+get_ipython().run_line_magic('history', '-n 1-5  # Show the first 5 commands in this session')
+get_ipython().run_line_magic('history', '-n 1-10  # Show the **** 5 commands in this session')
+get_ipython().run_line_magic('history', '-n 10  # Show the **** 5 commands in this session')
+get_ipython().run_line_magic('history', '-n 10-1  # Show the **** 5 commands in this session')
+get_ipython().run_line_magic('history', '-n 1-10  # Show the **** 5 commands in this session')
+# Save history to a file
+get_ipython().run_line_magic('save', 'last10.py 1-10')
